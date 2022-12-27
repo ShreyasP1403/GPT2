0983bded6fb03391a1134208eb1e760052ebd4ea
Srushti Salvi <srushtisalvi4@gmail.com>
2022-12-28 01:27:04 +0530

    login fixed

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword,} from "firebase/auth";
+import {getFirestore} from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,22 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  
+};
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleButton from 'react-google-button'
 import {Link} from 'react-router-dom'
 import './Login.css'
-import {auth} from '../firebase'
+import {auth, logInWithEmailAndPassword} from '../firebase'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {GoogleAuthProvider, signInWithRedirect} from 'firebase/auth'
 
 
-const googleSignIn =() =>{
-    const provider = new GoogleAuthProvider()
-    signInWithRedirect(auth, provider)
-}
+
 const Login = () => {
     const [user] = useAuthState(auth)
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     console.log(user)
+
+    const googleSignIn =() =>{
+        const provider = new GoogleAuthProvider()
+        signInWithRedirect(auth, provider)
+    }
+
     return (
         <div className='login'>
             <div className="container">
@@ -22,15 +29,15 @@ const Login = () => {
                         <h1><span>Log In</span></h1>
                         <div>
                             <label>Email</label>
-                            <input type="email" placeholder='Enter your Email' autoComplete='email'/>
+                            <input type="email" placeholder='Enter your Email' autoComplete='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div>
                             <label>Password</label>
-                            <input type="text" placeholder='Enter your Password' autoComplete='current-password'/>
+                            <input type="text" placeholder='Enter your Password' autoComplete='current-password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
                         </div>
-                        <button>Log IN</button>
+                        <button onClick={() => logInWithEmailAndPassword(email, password)}>Log IN</button>
                         <Link to="/signup">
-                        <button>Sign Up</button>
+                        <button >Sign Up</button>
                     </Link>
                         <h1 className='p1'>OR...</h1>
                         <GoogleButton onClick={googleSignIn} />
